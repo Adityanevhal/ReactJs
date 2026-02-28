2fd52c5726d613de01c4f757fabeb1ebfa36a561
Adityanevhal <adityanevhal1@gmail.com>
2026-03-01 01:27:05 +0530

    Array night

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-
 import Card from './components/Card'
+import User from './components/User'
 const App = () => {
+
+const name = ['Aditya', 'Arnav','Anushka','Anvit']
+
   return (
     <div className='parent'>
-      <Card /> 
-      <Card />
-      
-      
+      {name.map(function(){
+        return hello
+      })}
+        
+    
     </div>
   )
 }
